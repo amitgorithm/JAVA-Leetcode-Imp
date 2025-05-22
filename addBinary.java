17f795dadadf3c252f2dc392d0528e76821f3672
Amit Jape <japeamit@gmail.com>
2025-05-23 02:10:32 +0530

    Update and rename addBinary to addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -1,10 +1,10 @@
 //  Given two binary strings a and b, return their sum as a binary string.
 
-Constraints:
+// Constraints:
 
-1 <= a.length, b.length <= 104
-a and b consist only of '0' or '1' characters.
-Each string does not contain leading zeros except for the zero itself.
+// 1 <= a.length, b.length <= 104
+// a and b consist only of '0' or '1' characters.
+// Each string does not contain leading zeros except for the zero itself.
 
 class Solution {
     public String addBinary(String a, String b) {
