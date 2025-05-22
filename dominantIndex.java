956fbe8031c815fedd86fbb4cd08e7f6bbc02b76
Amit Jape <japeamit@gmail.com>
2025-05-23 02:13:49 +0530

    Update and rename dominantIndex to dominantIndex.java

--- dominantIndex.java
+++ dominantIndex.java
@@ -1,6 +1,6 @@
 // You are given an integer array nums where the largest integer is unique.
 
-Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise. //
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise. //
 
 public class Solution {
     public int dominantIndex(int[] nums) {
