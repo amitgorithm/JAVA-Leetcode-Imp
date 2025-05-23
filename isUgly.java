944eee5677a9270bcd609eda1956ee66656c703e
Amit Jape <japeamit@gmail.com>
2025-05-23 11:38:29 +0530

    Update and rename isUgly to isUgly.java

--- isUgly.java
+++ isUgly.java
@@ -1,7 +1,7 @@
 // write leetcode acceptable java program for the problem:
-An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+// An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
 
-Given an integer n, return true if n is an ugly number.
+//  Given an integer n, return true if n is an ugly number.
 
 class Solution {
     public boolean isUgly(int n) {
