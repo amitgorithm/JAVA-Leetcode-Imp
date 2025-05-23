3994db415bf8326a76b90202b67a92cd7e6f2712
Amit Jape <japeamit@gmail.com>
2025-05-23 11:41:32 +0530

    Update and rename removeDuplicates to removeDuplicates.java

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,23 +1,23 @@
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
-Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
-Return k.
-Custom Judge:
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
 
-The judge will test your solution with the following code:
+// The judge will test your solution with the following code:
 
-int[] nums = [...]; // Input array
-int[] expectedNums = [...]; // The expected answer with correct length
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
 
-int k = removeDuplicates(nums); // Calls your implementation
+// int k = removeDuplicates(nums); // Calls your implementation
 
-assert k == expectedNums.length;
-for (int i = 0; i < k; i++) {
-    assert nums[i] == expectedNums[i];
-}
-If all assertions pass, then your solution will be accepted. //
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted. //
 
 public class Solution {
     public int removeDuplicates(int[] nums) {
