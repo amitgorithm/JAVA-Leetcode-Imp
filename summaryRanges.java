3415930fe00dec03f45249a87d83405a6053b1c1
Amit Jape <japeamit@gmail.com>
2025-05-29 00:47:58 +0530

    Create summaryRanges.java

---
+++ summaryRanges.java
@@ -0,0 +1,33 @@
+/* You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == bb*/
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) return result;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            // Check end of array or break in sequence
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+                if (i < nums.length) start = nums[i];  // Set new start
+            }
+        }
+
+        return result;
+    }
+}
