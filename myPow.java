3332ec2d7113841b2b3086e912c3277947a38ffb
Amit Jape <japeamit@gmail.com>
2025-07-10 23:10:55 +0530

    Create myPow.java

---
+++ myPow.java
@@ -0,0 +1,31 @@
+
+/* Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+n is an integer.
+Either x is not zero or n > 0.
+-104 <= xn <= 104 */
+
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
