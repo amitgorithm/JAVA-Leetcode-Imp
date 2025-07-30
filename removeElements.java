a66651238a4ae2d75bc5b84685bb5b048c75883b
Amit Jape <japeamit@gmail.com>
2025-07-30 23:50:25 +0530

    Create removeElements.java

---
+++ removeElements.java
@@ -0,0 +1,17 @@
+/* Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head. */
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return dummy.next;
+    }
+}
