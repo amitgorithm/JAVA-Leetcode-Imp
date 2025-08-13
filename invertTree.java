14442d74c174977d062d6b701576f65a466b8344
Amit Jape <japeamit@gmail.com>
2025-08-13 23:56:33 +0530

    Create invertTree.java

---
+++ invertTree.java
@@ -0,0 +1,32 @@
+/* Given the root of a binary tree, invert the tree, and return its root. */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        // Swap the left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        // Recursively invert the left and right subtrees
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return root;
+    }
+}
