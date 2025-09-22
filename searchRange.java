71a912446d0ec7f812798f8f9dec43456ac1220e
Amit Jape <japeamit@gmail.com>
2025-09-22 23:54:36 +0530

    Create searchRange.java

---
+++ searchRange.java
@@ -0,0 +1,30 @@
+/** Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value. If target is not found in the array, return [-1, -1]. You must write an algorithm with O(log n) runtime complexity.
+**/
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[]{first, last};
+    }
+
+    private int findBound(int[] nums, int target, boolean isFirst) {
+        int left = 0, right = nums.length - 1, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                if (isFirst) {
+                    right = mid - 1; // keep looking on left
+                } else {
+                    left = mid + 1;  // keep looking on right
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
