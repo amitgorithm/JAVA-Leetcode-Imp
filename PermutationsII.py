ffbc6c722e15dcf2d0d813db0c9022ee6064ae18
Amit Jape <japeamit@gmail.com>
2025-11-15 23:53:41 +0530

    python code for unique permutations

---
+++ PermutationsII.py
@@ -0,0 +1,35 @@
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+
+                used[i] = True
+                backtrack(path + [nums[i]])
+                used[i] = False
+        
+        backtrack([])
+        return res
